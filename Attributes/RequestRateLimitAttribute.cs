da3b240a3fc00cb37ff8590e14efa4874ef8e270
parisajabbari <59807156+parisajabbari@users.noreply.github.com>
2020-02-18 22:00:10 +1100

    comment fix

--- Attributes/RequestRateLimitAttribute.cs
+++ Attributes/RequestRateLimitAttribute.cs
@@ -4,25 +4,20 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Caching.Memory;
 
-
 namespace TodoApi.Attributes
-{
-    
+{   
     [AttributeUsage(AttributeTargets.Method)]
 
-    //Action filters : Their execution surrounds the execution of action methods.
-    //The framework provides an abstract ActionFilterAttribute that can be subclassed.
-    //https://docs.microsoft.com/en-us/aspnet/core/mvc/controllers/filters?view=aspnetcore-3.1#action-filters
+    //Action filters : Their execution surrounds the execution of action methods.  
     public class RequestRateLimitAttribute : ActionFilterAttribute
     {
         public string Name{get; set;}
         public int Seconds{get; set;}
 
         private static MemoryCache Cache { get; } = new MemoryCache(new MemoryCacheOptions());
-        int RequestCounter = 0; 
+        int RequestCounter = 0;
     
         //OnActionExecuting method : Called before the action executes, after model binding is complete.
-        //https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.mvc.filters.iactionfilter.onactionexecuting?view=aspnetcore-3.1#Microsoft_AspNetCore_Mvc_Filters_IActionFilter_OnActionExecuting_Microsoft_AspNetCore_Mvc_Filters_ActionExecutingContext_
         public override void OnActionExecuting(ActionExecutingContext context)
         {            
             var ipAddress = context.HttpContext.Request.HttpContext.Connection.RemoteIpAddress;
@@ -31,6 +26,7 @@ namespace TodoApi.Attributes
             //Gets the item associated with this key if present.
             if(!Cache.TryGetValue(MemoryCacheKey, out bool entry))
             {    
+                //Reset counter
                 RequestCounter = 0;
                 
                 //set and entry option with expiration time on memory cache 
